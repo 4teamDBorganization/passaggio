8fc82ca9cde40bcdf3c71de6a030f44f47d9cb0d
Ahnhyojun <hyojunahn111@naver.com>
2024-04-10 20:08:21 +0900

    SignUpController 구현 #6

--- src/main/java/com/passaggio/project/controller/SignUpController.java
+++ src/main/java/com/passaggio/project/controller/SignUpController.java
@@ -1,11 +1,14 @@
 package com.passaggio.project.controller;
 
+import com.passaggio.project.models.signup.SignUpDTO;
 import com.passaggio.project.models.signup.SignUpService;
 import com.passaggio.project.view.signup.SignUp;
 
+import java.util.Map;
+
 public class SignUpController {
-    private final SignUp signUp;
-    private final SignUpService signUpService;
+    public final SignUp signUp;
+    public final SignUpService signUpService;
 
     public SignUpController(){
         signUp =new SignUp();
@@ -13,8 +16,22 @@ public class SignUpController {
 
     }
 
-    private void regispterSignU(){
-//        SignUpService signUpService
-    }
 
+    public void registerSignUp(Map<String, String> parameter) {
+
+        int userCode = Integer.parseInt(parameter.get("userCode"));
+        String userId = parameter.get("userId");
+        String userPwd = parameter.get("userPwd");
+        String userNickName = parameter.get("userNickName");
+        char userGender = parameter.get("userGender").charAt(0);
+
+        SignUpDTO signUpDTO = new SignUpDTO();
+        signUpDTO.setSeq(userCode);
+        signUpDTO.setId(userId);
+        signUpDTO.setPwd(userPwd);
+        signUpDTO.setNick(userNickName);
+        signUpDTO.setGender(userGender);
+
+        signUpService.registerUser(signUpDTO);
+    }
 }
