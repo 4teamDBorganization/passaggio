6b0f9611f0b3617f521d418b29394f2d1b18eb9d
Ahnhyojun <hyojunahn111@naver.com>
2024-04-10 20:14:49 +0900

    SQL 쿼리를 실행하게 만듬 #7

--- src/main/java/com/passaggio/project/mapper/LoginMapper.java
+++ src/main/java/com/passaggio/project/mapper/LoginMapper.java
@@ -3,6 +3,5 @@ package com.passaggio.project.mapper;
 import com.passaggio.project.models.signup.SignUpDTO;
 
 public interface LoginMapper {
-
-    int userCheck(SignUpDTO signUpDTO);
+    void insertUser(SignUpDTO signUpDTO);
 }
