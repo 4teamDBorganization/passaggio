911f5e6ae06ff45e056511a1f0dbe16da7f8d836
Ahnhyojun <hyojunahn111@naver.com>
2024-04-11 00:19:18 +0900

    로그인시에 while문을 벗어나고 마이페이지로 이동하게 만듬 #14

--- src/main/java/com/passaggio/project/Application/Application.java
+++ src/main/java/com/passaggio/project/Application/Application.java
@@ -2,6 +2,7 @@ package com.passaggio.project.Application;
 
 import com.passaggio.project.controller.LoginController;
 import com.passaggio.project.controller.SignUpController;
+import com.passaggio.project.myPage.MypageService;
 import com.passaggio.project.view.login.Login;
 import com.passaggio.project.view.signup.SignUp;
 
@@ -13,12 +14,14 @@ public class Application {
 
         LoginController loginController = new LoginController();
         SignUpController signUpController = new SignUpController();
+        MypageService mypageService = new MypageService();
 
+        boolean ing = true;
         Scanner sc = new Scanner(System.in);
 
         //만약 1번을 선택하면 로그인창으로 이동하게
         //2번을 선택한다면 회원가입창으로
-        while (true) {
+        while (ing) {
             System.out.println("계정이 있으십니까?");
             System.out.println("1. 예(로그인 창으로 이동됩니다.)");
             System.out.println("2. 아니요(회원가입으로 이동됩니다.)");
@@ -33,7 +36,7 @@ public class Application {
                 Login loginService = new Login();
                 Map<String, String> loginInfo = loginService.inputMenu();
                 loginController.goLogin(loginInfo);
-
+                ing = false;
             } else if (choose == 2) {
                 System.out.println("회원 가입을 시작합니다.");
                 Map<String, String> signUpInfo = SignUp.inputMenu();
@@ -44,6 +47,7 @@ public class Application {
                 break;
             }
         }
+        mypageService.showMypage();
 
     }
 
