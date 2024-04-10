dee0eb9a3462138107b004d9ac7f0940ba747356
Ahnhyojun <hyojunahn111@naver.com>
2024-04-10 20:22:02 +0900

    사용자로부터 입력 받은 정보를 파라미터에 저장함 #9

--- src/main/java/com/passaggio/project/view/signup/SignUp.java
+++ src/main/java/com/passaggio/project/view/signup/SignUp.java
@@ -1,60 +1,52 @@
 package com.passaggio.project.view.signup;
 
+import com.passaggio.project.controller.SignUpController;
 import com.passaggio.project.models.signup.SignUpDTO;
 import com.passaggio.project.view.login.Login;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Timer;
 
 public class SignUp {
+    /*public static void userCheck() {
+        SignUpController signUpController = new SignUpController();
 
-    public static void userCheck() {
         Scanner sc = new Scanner(System.in);
         Timer timer = new Timer();
         int loginNum = sc.nextInt();
 
+    }*/
 
-            //int loginNum = Integer.parseInt(loginNumString);        //String을 숫자로 변환함
+    public static Map<String, String> inputMenu(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("회원번호를 입력해주세요: ");
+        String seq = sc.nextLine();
 
-            if (loginNum == 1) {
-                Login loginService = new Login();
-                loginService.login();
+        System.out.print("아이디를 입력해주세요: ");
+        String id = sc.nextLine();
 
-//                    WrongNum = false;
-            } else if (loginNum == 2) {
+        System.out.print("비밀번호를 입력해주세요: ");
+        String pwd = sc.nextLine();
 
+        System.out.print("닉네임을 입력해주세요: ");
+        String nick = sc.nextLine();
 
-                System.out.println("회원 가입을 시작합니다.");
+        System.out.print("성별(M/F)을 입력해주세요: ");
+        String gender = sc.nextLine();
+//        char gender = sc.next().charAt(0);
 
-                System.out.print("회원번호를 입력해주세요: ");
-                int seq = sc.nextInt();
+        Map<String, String> parameter = new HashMap<>();
 
-                System.out.print("아이디를 입력해주세요: ");
-                sc.nextLine();
-                String id = sc.nextLine();
+        parameter.put("userCode", seq);
+        parameter.put("userId", id);
+        parameter.put("userPwd", pwd);
+        parameter.put("userNickName", nick);
+        parameter.put("userGender", gender);
 
-                System.out.print("비밀번호를 입력해주세요: ");
-                String pwd = sc.nextLine();
-
-                System.out.print("닉네임을 입력해주세요: ");
-                String nick = sc.nextLine();
-
-                System.out.print("성별(M/F)을 입력해주세요: ");
-                char gender = sc.next().charAt(0);
-
-                SignUpDTO signUpDTO = new SignUpDTO(seq, id, pwd, nick, gender);      //signup객체생성
-
-                // 데이터베이스 연결
-
-//                            int result = pstmt.executeUpdate();
-//                            if (result > 0) {
-//                                System.out.println("회원 가입에 성공했습니다.");
-////                                return false;
-//                            } else {
-//                                System.out.println("회원 가입에 실패했습니다.");
-//                            }
-            }
-        }
+        return parameter;
+    }
 
 }
 
