9e6e9bb19f9ac0efa6b639c0cca2d6ec1b19337b
Ahnhyojun <hyojunahn111@naver.com>
2024-04-14 03:04:00 +0900

    LostAccountService 생성 #33

---
+++ src/main/java/com/passaggio/project/view/lostaccount/LostAccountService.java
@@ -0,0 +1,17 @@
+package com.passaggio.project.view.lostaccount;
+
+import com.passaggio.project.mapper.LoginMapper;
+import com.passaggio.project.models.signup.SignUpDTO;
+import org.apache.ibatis.session.SqlSession;
+
+import static com.passaggio.project.common.Template.getSqlSession;
+
+public class LostAccountService {
+    public static boolean findmyid(int seq, String nick, String gender) {
+        try (SqlSession sqlSession = getSqlSession()) {
+            LoginMapper mapper = sqlSession.getMapper(LoginMapper.class);
+            SignUpDTO findId = mapper.FindAccountId(seq, nick, gender);
+            return findId != null; //false 반환
+        }
+    }
+}
