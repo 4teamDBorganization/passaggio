116eaf594615755d6e0a163104c5488ef198816e
Ahnhyojun <hyojunahn111@naver.com>
2024-04-14 03:03:11 +0900

    loginmapper.xml #33

--- src/main/java/com/passaggio/project/mapper/LoginMapper.java
+++ src/main/java/com/passaggio/project/mapper/LoginMapper.java
@@ -1,6 +1,7 @@
 package com.passaggio.project.mapper;
 
 import com.passaggio.project.models.signup.SignUpDTO;
+import org.apache.ibatis.annotations.Param;
 
 public interface LoginMapper {
     void insertUser(SignUpDTO signUpDTO);
@@ -8,6 +9,7 @@ public interface LoginMapper {
     String whatIsId(String id);
     String whatIsNick(String nick);
     String whatIsMyNick(String nick);
+    SignUpDTO FindAccountId(@Param("seq")int seq, @Param("nick")String nick, @Param("gender")String gender);
 
     SignUpDTO gologin(SignUpDTO signUpDTO);
 
